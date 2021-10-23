b4acafe4331e0f072e11291238185037c8b82274
yunusarikan <yunusziya_arikan@hotmail.com>
2021-10-23 18:33:03 +0300

    For dtos.

---
+++ DeliveryApp.Shared/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace DeliveryApp.Shared.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ProductBrand { get; set; }
+        public string ProductType { get; set; }        
+        public string PictureUrl { get; set; }
+
+
+    }
+}
---
+++ DeliveryApp.Shared/Dtos/ProductListDto.cs
@@ -0,0 +1,16 @@
+﻿using DeliveryApp.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Shared.Dtos
+{
+    public class ProductListDto
+    {
+        public IList<Product> Products { get; set; }
+        public int? BrandId { get; set; }
+
+    }
+}
