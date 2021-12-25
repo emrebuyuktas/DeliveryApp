0e2a968430401e675b1b792b85bb1e37c3597dbd
emrebuyuktas <emrebyk348@gmail.com>
2021-12-25 22:24:46 +0300

    SomeChanges

--- DeliveryApp.Web/Models/Category.cs
+++ DeliveryApp.Web/Models/Category.cs
@@ -2,8 +2,6 @@
 using DeliveryApp.Shared.Result.ComplexTypes;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DeliveryApp.Web.Models
 {
