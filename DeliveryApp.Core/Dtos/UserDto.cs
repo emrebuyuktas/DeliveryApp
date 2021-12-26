1e33f60c1e2a9206edca7f9dc04aa0ded8963886
emrebuyuktas <emrebyk348@gmail.com>
2021-12-26 15:40:35 +0300

    some changes

--- DeliveryApp.Core/Dtos/UserDto.cs
+++ DeliveryApp.Core/Dtos/UserDto.cs
@@ -8,6 +8,7 @@ namespace DeliveryApp.Core.Dtos
 {
     public class UserDto
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
